1f7f556ea0fb60c870df184b1a007bbe13431f81
visionalee <flook.xtnt@gmail.com>
2025-12-16 16:16:08 +0700

    lab8_3 completed

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -11,3 +11,46 @@
 "Brook"
 "Jinbe"
 */
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string character;
+    int age, height, bounty;
+    cout << "Enter your age: ";
+    cin >> age;
+    if (age <= 25)
+    {
+        cout << "Enter your height: ";
+        cin >> height;
+        if (height < 100)
+            character = "Chopper";
+        else if (height < 180)
+            character = "Usopp";
+        else
+        {
+            cout << "Enter your bounty: ";
+            cin >> bounty;
+            if (bounty > 1100000000)
+                character = "Zoro";
+            else
+                character = "Sanji";
+        }
+    }
+    else if (age <= 60)
+    {
+        cout << "Enter your bounty: ";
+        cin >> bounty;
+        if (bounty > 500000000)
+            character = "Jinbe";
+        else
+            character = "Franky";
+    }
+    else
+        character = "Brook";
+
+    cout << "Your character = " << character;
+}
